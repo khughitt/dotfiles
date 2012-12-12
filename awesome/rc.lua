54016d924faa7232b90453c1311f280831bd9933
Keith Hughitt <keith.hughitt@gmail.com>
2012-12-12 11:23:52 -0500

    Updated awesome config to include run_once function from Arch wiki; added tptoggle to autostart list to keep touchpad off

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -571,7 +571,27 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 -- }}}
 
 -- Autostart
-awful.util.spawn_with_shell("nm-applet")
-awful.util.spawn_with_shell("redshift -l 37.052328:-78.662109")
-awful.util.spawn_with_shell("dropboxd")
+function run_once(prg,arg_string,pname,screen)
+    if not prg then
+        do return nil end
+    end
+
+    if not pname then
+       pname = prg
+    end
+
+    if not arg_string then 
+        awful.util.spawn_with_shell("pgrep -f -u $USER -x '" .. pname .. "' || (" .. prg .. ")",screen)
+    else
+        awful.util.spawn_with_shell("pgrep -f -u $USER -x '" .. pname .. "' || (" .. prg .. " " .. arg_string .. ")",screen)
+    end
+end
+
+--run_once("xscreensaver","-no-splash")
+--run_once("pidgin",nil,nil,2)
+--run_once("wicd-client",nil,"/usr/bin/python2 -O /usr/share/wicd/gtk/wicd-client.py")
+run_once("nm-applet")
+run_once("redshift -l 37.052328:-78.662109")
+run_once("dropboxd")
+run_once("tptoggle")
 
