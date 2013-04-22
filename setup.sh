a5b4437e71a5e50a9626ff036965114fced0cae6
Keith Hughitt <keith.hughitt@gmail.com>
2013-04-22 13:34:15 -0400

    Few small changes

--- setup.sh
+++ setup.sh
@@ -7,6 +7,13 @@ if [ "$SH" != "bash" ] && [ "$SH" != "zsh" ]; then
     exit;
 fi
 
+# X11
+read -p "Install GUI dot files? [yes/no]: " GUI
+if [ "$GUI" != "yes" ] && [ "$GUI" != "no" ]; then
+    echo "Invalid choice. Exiting..."
+    exit;
+fi
+
 # Check for configuration directory
 if [ -z $XDG_CONFIG_HOME ]; then
     XDG_CONFIG_HOME=$HOME/.config
@@ -55,7 +62,7 @@ ln_s ${PWD}/awesome ${XDG_CONFIG_HOME}/awesome
 cp -r ${PWD}/gedit/styles ${XDG_CONFIG_HOME}/gedit/
 
 # Everything else
-for path in "devilspie" "conky" "conkyrc" "gitconfig" "gitignore_global" "Rprofile" "vim" "vimrc" "Xdefaults"; do
+for path in "devilspie" "conky" "conkyrc" "gitconfig" "gitignore_global" "Rprofile" "vim" "vimrc" "Xresources"; do
     ln_s ${PWD}/${path} ~/.${path}
 done
 
