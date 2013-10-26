1095acd4dfece364469a96b49f2797157a990cd0
Keith Hughitt <keith.hughitt@gmail.com>
2013-10-26 07:54:02 -0400

    Added markdown plugin for vim

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -71,7 +71,7 @@ local layouts =
 {
     awful.layout.suit.floating,
     --awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
+    awful.layout.suit.tile.right,
     awful.layout.suit.tile.bottom,
     --awful.layout.suit.tile.top,
     awful.layout.suit.fair,
