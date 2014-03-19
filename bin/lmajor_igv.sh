b105d55793ff2587912bf6390acef33789ff9283
Keith Hughitt <keith.hughitt@gmail.com>
2014-03-19 16:00:16 -0400

    Added helper script to launch IGV with L. major genome

---
+++ bin/lmajor_igv.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+ANNOTATIONS=${REF}/lmajor_friedlin/annotation
+IGV_VERSION="2.3.32"
+
+java -Xmx12096m -Djava.net.preferIPv4Stack=true \
+    -jar ${HOME}/software/IGV_${IGV_VERSION}/igv.jar \
+    -g ${REF}/lmajor_friedlin/genome/TriTrypDB-7.0_LmajorFriedlin_Genome.fasta \
+       ${REF}/lmajor_friedlin/annotation/TriTrypDB-7.0_LmajorFriedlin_genes.gff $*
