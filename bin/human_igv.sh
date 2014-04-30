7d2b045a95e38453262a18d092683851a5c563da
Keith Hughitt <keith.hughitt@gmail.com>
2014-04-30 08:36:08 -0400

    Updated human IGV launcher

--- bin/human_igv.sh
+++ bin/human_igv.sh
@@ -4,4 +4,4 @@ IGV_VERSION="2.3.32"
 java -Xmx12096m -Djava.net.preferIPv4Stack=true \
     -jar ${HOME}/software/IGV_${IGV_VERSION}/igv.jar \
     -g ${REF}/hsapiens/genome/hg19.fasta  \
-       ${REF}/hsapiens/annotation/ensembl/release-75/Homo_sapiens.GRCh37.75.compat.gtf,$*
+       ${REF}/hsapiens/annotation/ensembl/release-75/Homo_sapiens.GRCh37.75.compat.sorted.gtf,$*
