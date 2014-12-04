a1f7e6166b87d5fc20136bdcf1eb89a8598029fb
Keith Hughitt <keith.hughitt@gmail.com>
2014-12-04 07:27:51 -0500

    various updates

--- bin/igv_lmajor.sh
+++ bin/igv_lmajor.sh
@@ -2,6 +2,10 @@
 IGV_VERSION="2.3.36"
 TRITRYPDB_VERSION="8.0"
 
+export JAVA_FONTS=/usr/share/fonts/TTF
+export _JAVA_AWT_WM_NONREPARENTING=1
+export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
+
 java -Xmx12096m -Djava.net.preferIPv4Stack=true \
     -jar ${HOME}/software/IGV_${IGV_VERSION}/igv.jar \
     -g ${REF}/lmajor_friedlin/genome/TriTrypDB-${TRITRYPDB_VERSION}_LmajorFriedlin_Genome.fasta \
--- bin/igv_mtb.sh
+++ bin/igv_mtb.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 IGV_VERSION="2.3.36"
 
+export JAVA_FONTS=/usr/share/fonts/TTF
+export _JAVA_AWT_WM_NONREPARENTING=1
+export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
+
 java -Xmx12096m -Djava.net.preferIPv4Stack=true \
     -jar ${HOME}/software/IGV_${IGV_VERSION}/igv.jar \
     -g ${REF}/mtuberculosis/genome/mycobacterium_tuberculosis_h37rv_2_supercontigs.fasta \
