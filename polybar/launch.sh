7f58e1ce9b6e7bede657eef748654063f5544b6e
Keith Hughitt <keith.hughitt@gmail.com>
2020-01-06 11:41:29 -0500

    Added alternate polybar config for laptop

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,11 +6,21 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+# Determine config file to use
+if [[ `hostname` == "Europa" ]]; then
+    CFG="$HOME/.config/polybar/config.europa"
+else
+    CFG="$HOME/.config/polybar/config"
+fi
+
+echo `hostname`
+echo $CFG
+
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload bar &
+    MONITOR=$m polybar --config=$CFG --reload bar &
   done
 else
-  polybar --reload bar &
+  polybar --config=$CFG --reload bar &
 fi
 
