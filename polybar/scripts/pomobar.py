e64bc1e4be0958625ab6c0fa47cbbd599dc814ff
Keith Hughitt <keith.hughitt@gmail.com>
2021-02-07 23:56:13 -0500

    feb 7 update

--- polybar/scripts/pomobar.py
+++ polybar/scripts/pomobar.py
@@ -38,7 +38,7 @@ if config.has_option("PATH", "DATABASE_PATH"):
 class Pomodoro:
     """Normal pomodoro"""
 
-    def __init__(self, duration=55):
+    def __init__(self, duration=40):
         self.duration = int(duration)
         self.ICON = "%{u#8FFFA6} "
 
