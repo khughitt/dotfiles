f53e4afd29d1ceed64972b907afb8a2a236005dc
Keith Hughitt <keith.hughitt@gmail.com>
2021-09-05 06:14:01 -0700

    rofi, polybar, etc.

--- rofi/scripts/rofi-tasks.sh
+++ rofi/scripts/rofi-tasks.sh
@@ -3,7 +3,12 @@
 # rofi tasks launcher
 # kh (jan 2021)
 #
-tasks=(abstract-algebra bayes complexity dsp gene-sets shaders test three.js)
+tasks=(abstract-algebra bayes complexity dsp gen-art gene-sets l-systems shaders planets three.js)
+
+# alternate colorschemes to use
+ALT1="palenight"
+ALT2="nord"
+ALT3="onedark"
 
 if [ -z $@ ]; then
     print -l "${tasks[@]}"
@@ -51,7 +56,6 @@ else
         # audio signal processing
         i3empty.py next 1 > /dev/null
         i3-msg layout splith > /dev/null
-
         coproc kitty -d $NOTES/courses/audio-signal-processing nvim audio-signal-processing.md > /dev/null
         coproc google-chrome-stable https://www.coursera.org/learn/audio-signal-processing/home/welcome > /dev/null
         # i3-msg '[class="^Nautilus$"] focus'
@@ -61,13 +65,33 @@ else
         # co-expression networks 
         i3empty.py next 1 > /dev/null
         i3-msg layout splith > /dev/null
-
         coproc kitty -d $NOTES/tasks nvim task-coex.md > /dev/null
     elif [ "$@" = "three.js" ]; then
         i3empty.py next 1 > /dev/null
         i3-msg layout stacking > /dev/null
         coproc kitty -d ~/d/three-js > /dev/null
         coproc kitty -d $NOTES/tech/web/ nvim three-js.md > /dev/null
-        coproc firefox -new-window https://threejs.org/docs/index.html#manual/en/introduction/Creating-a-scene > /dev/null
+        coproc firefox -new-window "https://threejs.org/docs/index.html#manual/en/introduction/Creating-a-scene" > /dev/null
+    elif [ "$@" = "planets" ]; then
+        i3empty.py next 1 > /dev/null
+        i3-msg layout stacking > /dev/null
+        coproc kitty -d ~/d/three-js/00-sphere/src nvim App.js > /dev/null
+        coproc kitty -d ~/d/three-js/00-sphere yarn start > /dev/null
+        coproc kitty -d $NOTES/tech/procedural-generation/ nvim planets.md > /dev/null
+        coproc firefox -new-window "https://threejs.org/docs/index.html#manual/en/introduction/Creating-a-scene" > /dev/null
+    elif [ "$@" = "gen-art" ]; then
+        i3empty.py next 1 > /dev/null
+        i3-msg layout stacking > /dev/null
+        coproc kitty -d ~/d/gen-art/01-hi nvim index.html > /dev/null
+        coproc kitty -d ~/d/gen-art/01-hi python -m 'http.server' 8001 > /dev/null
+        coproc kitty -d $NOTES/art/generative-art/ nvim generative-art.md > /dev/null
+    elif [ "$@" = "l-systems" ]; then
+        i3empty.py next 1 > /dev/null
+        i3-msg layout stacking > /dev/null
+        coproc kitty -d ~/d/l-systems/00-hello nvim index.html > /dev/null
+        coproc kitty -d ~/d/l-systems/00-hello python -m 'http.server' 8001 > /dev/null
+        coproc zathura ~/d/books/abop.pdf > /dev/null
+        coproc kitty -d $NOTES/books nvim -c "colorscheme $ALT2" algorithmic-beauty-of-plants.md > /dev/null
+        coproc kitty -d $NOTES/tech/procedural-generation/nature nvim -c "colorscheme $ALT1" l-systems.md > /dev/null
     fi
 fi 
