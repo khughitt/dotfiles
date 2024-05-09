446a66c534c70b67fee167d7d6c449b2aac8bee7
Keith Hughitt <keith.hughitt@gmail.com>
2024-05-09 01:33:17 -0400

    add mambarc to setup.sh

--- setup.sh
+++ setup.sh
@@ -83,6 +83,9 @@ ln_s ${DOTS_HOME}/$julia/startup.jl ${HOME}/.julia/config/startup.jl
 # jupyter
 ln_s ${DOTS_HOME}/jupyter/jupyter_qtconsole_config.py ${HOME}/.jupyter/jupyter_qtconsole_config.py
 
+# mamba
+ln_s ${DOTS_HOME}/condarc ${HOME}/.mambarc
+
 # r
 ln_s ${DOTS_HOME}/lintr ${HOME}/.lintr
 
