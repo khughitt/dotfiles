dec251b347f4d66476c415359f245d67bbabe40b
Keith Hughitt <keith.hughitt@gmail.com>
2025-02-20 07:57:56 -0500

    nvim updates

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -1,13 +1,13 @@
 return {
   -- colorschemes
-  -- {'navarasu/onedark.nvim'},
   {'folke/tokyonight.nvim', lazy = false, priority = 1000, opts = {}, },
-  --'drewtempelmeyer/palenight.vim'
-  --'rakr/vim-one'
-  --'reedes/vim-colors-pencil'
-  --'tyrannicaltoucan/vim-quantum'
-  --{'dracula/vim', name = 'dracula'}
-  --{"scottmckendry/cyberdream.nvim", lazy = false}
+  -- {'navarasu/onedark.nvim'},
+  -- {'drewtempelmeyer/palenight.vim'},
+  -- {'rakr/vim-one'},
+  -- {'reedes/vim-colors-pencil'},
+  -- {'tyrannicaltoucan/vim-quantum'},
+  -- {'dracula/vim', name = 'dracula'},
+  -- {"scottmckendry/cyberdream.nvim", lazy = false},
 
   -- plugins
   {'JoosepAlviste/nvim-ts-context-commentstring'},
@@ -15,7 +15,6 @@ return {
   {'ervandew/supertab'},
   {'ggandor/leap.nvim'},
   {'godlygeek/tabular'},
-  {'junegunn/vim-emoji', lazy=true},
   {'lewis6991/gitsigns.nvim'},
   {'ncm2/float-preview.nvim'},
   {'norcalli/nvim-colorizer.lua', lazy=true},
@@ -26,6 +25,7 @@ return {
       require("telescope").load_extension "frecency"
     end,
   },
+  {'nvimtools/none-ls.nvim'},
   {'romgrk/barbar.nvim'},
   {'scrooloose/nerdcommenter'},
   {'tomtom/tlib_vim'},
@@ -38,25 +38,20 @@ return {
   { 'vladdoster/remember.nvim', config = [[ require('remember') ]] },
 
   -- language support
+  {"williamboman/mason.nvim"},
+  {"williamboman/mason-lspconfig.nvim"},
+  {"neovim/nvim-lspconfig"},
   {'JuliaEditorSupport/julia-vim', lazy=true},
   {'bioSyntax/bioSyntax-vim', lazy=true},
   {'chrisbra/csv.vim', lazy=true},
   {'fatih/vim-go', lazy=true},
   {'glench/vim-jinja2-syntax', lazy=true},
   {'ibab/vim-snakemake', lazy=true},
-  {'jalvesaq/Nvim-R'},
-  {'leafgarland/typescript-vim', lazy=true},
-  {'maxmellon/vim-jsx-pretty', lazy=true},
+  {"R-nvim/R.nvim", lazy = false},
   {'mboughaba/i3config.vim', lazy=true},
   {'mllg/vim-devtools-plugin', lazy=true},
   {'mzlogin/vim-markdown-toc', lazy=true},
-  {'pangloss/vim-javascript', lazy=true},
   {'raimon49/requirements.txt.vim', lazy=true},
-  {'stephpy/vim-yaml', lazy=true},
-  {'tikhomirov/vim-glsl', lazy=true},
-  {'udalov/kotlin-vim', lazy=true},
-  {'yuezk/vim-js', lazy=true},
-  { 'neoclide/coc.nvim', branch = 'release', lazy=true},
   {'snakemake/snakemake', 
     ft='snakemake', 
     config = function(plugin)
