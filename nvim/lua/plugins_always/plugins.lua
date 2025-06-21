ae5114d274e11bf64900ee00526780331e31d7ae
Keith Hughitt <keith.hughitt@gmail.com>
2025-06-21 15:21:53 -0400

    nvim

--- nvim/lua/plugins_always/plugins.lua
+++ nvim/lua/plugins_always/plugins.lua
@@ -1,29 +1,10 @@
 return {
   -- colorschemes
   {'folke/tokyonight.nvim', lazy = false, priority = 1000, opts = {}, },
-  -- {'navarasu/onedark.nvim'},
-  -- {'drewtempelmeyer/palenight.vim'},
-  -- {'rakr/vim-one'},
-  -- {'reedes/vim-colors-pencil'},
-  -- {'tyrannicaltoucan/vim-quantum'},
-  -- {'dracula/vim', name = 'dracula'},
-  -- {"scottmckendry/cyberdream.nvim", lazy = false},
 
   -- plugins
-  {'JoosepAlviste/nvim-ts-context-commentstring'},
-  {'chentoast/marks.nvim'},
   {'ggandor/leap.nvim'},
   {'godlygeek/tabular'},
-  {'lewis6991/gitsigns.nvim'},
-  {'ncm2/float-preview.nvim'},
-  {'norcalli/nvim-colorizer.lua', lazy=true},
-  {'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' }},
-  {'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-  {'nvim-telescope/telescope-frecency.nvim',
-    config = function()
-      require("telescope").load_extension "frecency"
-    end,
-  },
   {'scrooloose/nerdcommenter'},
   {'tomtom/tlib_vim'},
   {'tpope/vim-surround'},
@@ -35,6 +16,7 @@ return {
   { 'vladdoster/remember.nvim', config = [[ require('remember') ]] },
 
   -- textobjs
+  {'JoosepAlviste/nvim-ts-context-commentstring'},
   {'tyru/vim-textobj-underscore', branch = 'support-3-cases', dependencies = {'kana/vim-textobj-user'}},
   -- {'glts/vim-textobj-comment'}
 
