d159d49428b65d7ca703f3f2698628aef90f6204
Keith Hughitt <keith.hughitt@gmail.com>
2025-11-01 10:17:02 -0400

    disable telescope-fzf for now

--- nvim/lua/user/plugins_notvscode/plugins.lua
+++ nvim/lua/user/plugins_notvscode/plugins.lua
@@ -16,7 +16,7 @@ return {
   {'ncm2/float-preview.nvim'},
   {'norcalli/nvim-colorizer.lua', lazy=true},
   {'nvim-telescope/telescope.nvim', tag = '0.1.8', dependencies = { 'nvim-lua/plenary.nvim' }},
-  {'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+  -- {'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   {'nvim-telescope/telescope-frecency.nvim',
     config = function()
       require("telescope").load_extension "frecency"
