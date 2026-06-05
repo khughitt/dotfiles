01b18a8ded05bb6061bb9daffe7f9945d7abd193
Keith Hughitt <keith.hughitt@gmail.com>
2026-06-05 07:29:54 -0400

    chore: scripts clean-up

--- bin/archive/igv_tcruzi.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-IGV_VERSION="3.0_beta"
-
-java -Xmx12096m -Djava.net.preferIPv4Stack=true \
-    -jar ${HOME}/software/IGV_${IGV_VERSION}/igv.jar \
-    -g ${REF}/tcruzi_clbrener_esmeraldo-like/genome/TriTrypDB-30_TcruziCLBrenerEsmeraldo-like_Genome.fasta \
-       ${REF}/tcruzi_clbrener_esmeraldo-like/annotation/TriTrypDB-30_TcruziCLBrenerEsmeraldo-like.gff,$*
